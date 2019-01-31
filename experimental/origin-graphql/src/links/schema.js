49014a0df21a6894e7f770ab0da251e93f375cc1
Nick Poulden <nick@poulden.com>
2019-01-31 10:55:07 -0700

    Example remote schema

--- experimental/origin-graphql/src/links/schema.js
+++ experimental/origin-graphql/src/links/schema.js
@@ -1,8 +1,26 @@
-import { makeExecutableSchema } from 'graphql-tools'
+import { HttpLink } from 'apollo-link-http'
+import {
+  makeExecutableSchema,
+  makeRemoteExecutableSchema,
+  mergeSchemas
+} from 'graphql-tools'
 
 import typeDefs from '../typeDefs/index'
 import resolvers from '../resolvers/index'
+import growthTypeDefs from '../typeDefs/Growth'
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const growthSchema = makeExecutableSchema({
+  typeDefs: growthTypeDefs
+})
+
+const schema = mergeSchemas({
+  schemas: [
+    makeRemoteExecutableSchema({
+      schema: growthSchema,
+      link: new HttpLink({ uri: 'http://localhost:3005/graphql', fetch })
+    }),
+    makeExecutableSchema({ typeDefs, resolvers })
+  ]
+})
 
 export default schema
