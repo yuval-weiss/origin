41d2655c306a1191ac43e4ebf02f16bb69b5b63f
Nick Poulden <nick@poulden.com>
2019-01-31 11:24:28 -0700

    Example mock data

--- experimental/origin-graphql/src/links/schema.js
+++ experimental/origin-graphql/src/links/schema.js
@@ -2,25 +2,29 @@ import { HttpLink } from 'apollo-link-http'
 import {
   makeExecutableSchema,
   makeRemoteExecutableSchema,
-  mergeSchemas
+  mergeSchemas,
+  addMockFunctionsToSchema
 } from 'graphql-tools'
 
 import typeDefs from '../typeDefs/index'
 import resolvers from '../resolvers/index'
 import growthTypeDefs from '../typeDefs/Growth'
 
-const growthSchema = makeExecutableSchema({
-  typeDefs: growthTypeDefs
-})
+let growthSchema
+if (process.env.GROWTH) {
+  growthSchema = makeRemoteExecutableSchema({
+    schema: makeExecutableSchema({ typeDefs: growthTypeDefs }),
+    link: new HttpLink({ uri: 'http://localhost:3005/graphql', fetch })
+  })
+} else {
+  growthSchema = makeExecutableSchema({ typeDefs: growthTypeDefs })
+  addMockFunctionsToSchema({ schema: growthSchema, mocks: {
+    DateTime: () => 'Date'
+  } })
+}
 
 const schema = mergeSchemas({
-  schemas: [
-    makeRemoteExecutableSchema({
-      schema: growthSchema,
-      link: new HttpLink({ uri: 'http://localhost:3005/graphql', fetch })
-    }),
-    makeExecutableSchema({ typeDefs, resolvers })
-  ]
+  schemas: [growthSchema, makeExecutableSchema({ typeDefs, resolvers })]
 })
 
 export default schema
